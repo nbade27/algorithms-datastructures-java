4c3ab0b199679654d1f1bb54ba6767269cb1e53e
fullstacknaga <fullstacknaga@gmail.com>
2022-07-08 12:53:59 +0530

    recursion practice

---
+++ src/com/naga/revision/algorithms/Recursion.java
@@ -0,0 +1,22 @@
+package com.naga.revision.algorithms;
+
+public class Recursion {
+
+    public static void main(String[] args) {
+
+        //we must have breaking condition in recursion programming
+        //we will use factorial for example
+
+        int fact = factorial(19);
+        //getting 0 for 99 may be type issue
+        System.out.println(fact);
+    }
+
+    public static int factorial(int num){
+        if(num == 0) return 1;
+       // System.out.println(num);
+
+         return num * factorial(num - 1);
+
+    }
+}
