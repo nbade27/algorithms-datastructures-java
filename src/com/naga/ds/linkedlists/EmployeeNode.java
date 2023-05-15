0a15ed9681055665022ada1aa5b731b0105fd76e
fullstacknaga <fullstacknaga@gmail.com>
2023-05-15 12:24:51 +0530

    Adding linked list code

---
+++ src/com/naga/ds/linkedlists/EmployeeNode.java
@@ -0,0 +1,31 @@
+package com.naga.ds.linkedlists;
+
+public class EmployeeNode {
+
+    private Employee employee;
+    private EmployeeNode next;
+
+    public EmployeeNode(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public EmployeeNode getNext() {
+        return next;
+    }
+
+    public void setNext(EmployeeNode next) {
+        this.next = next;
+    }
+
+    public String toString() {
+        return employee.toString();
+    }
+}
